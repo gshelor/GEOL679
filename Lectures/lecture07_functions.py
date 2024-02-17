6dbaf420deac1d9b371ea81e165ca90e1c75c7b9
Griffin Shelor <griffinshelor@gmail.com>
2024-02-16 16:29:31 -0800

    updated through lecture 7 in-class exercise

---
+++ Lectures/lecture07_functions.py
@@ -0,0 +1,18 @@
+def glc1(a, b):
+    """
+    This program will take arguments a, b.
+    It will return a * b, a + b.
+    """  
+    c = a * b
+    d = a + b
+    return c, d
+
+def glc2(a, b):
+    """
+    This program will take arguments a, b.
+    It will return a / b, a - b.
+    """  
+    c = a / b
+    d = a - b
+    return c, d
+
---
+++ Lectures/lecture07_test.py
@@ -0,0 +1,3 @@
+x=[0, 1, 2, 3, 4, 5]
+for val in x:
+    print(val)
